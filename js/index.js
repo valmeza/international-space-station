cf6d3819de7d2b4b2ae2daa364b4d041a820ef75
Valeria Meza <valeriameza173@gmail.com>
2020-04-22 20:24:56 -0500

    following documentations instructions for this project

--- js/index.js
+++ js/index.js
@@ -1,28 +1,34 @@
-// "use strict";
+"use strict";
 
-// (function() {
-//     $(document).ready(function() {
-//         console.log('DOM Ready');
-//         function moveISS() {
-//             $.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
-//                 var lat = ['iss_position']['latitude'];
-//                 var lon = ['iss_position']['longitude'];
+(function () {
+  $(document).ready(function () {
+    console.log("DOM Ready");
+    function moveISS() {
+      $.getJSON("http://api.open-notify.org/iss-now.json?callback=?", function (
+        data
+      ) {
+        var lat = ["iss_position"]["latitude"];
+        var lon = ["iss_position"]["longitude"];
 
-//                 iss.setLatLng([lat, lon]);
-//                 isscirc.setLatLng([lat, lon]);
-//                 map.panTo([lat, lon], animate=true);
-//             });
-//             setTimeout(moveISS, 5000);
-//         }
-//     });
-// })();   
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+        iss.setLatLng([lat, lon]);
+        isscirc.setLatLng([lat, lon]);
+        map.panTo([lat, lon], (animate = true));
+      });
+      setTimeout(moveISS, 5000);
+    }
+    var mymap = L.map("mapid").setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1, 
+        accessToken: MAP_TOKEN,
+      }
+    ).addTo(mymap);
+  });
+})();
