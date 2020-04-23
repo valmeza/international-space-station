d18fdcf2a8a5c6f718823257e4d8c69ff83abf32
Valeria Meza <valeriameza173@gmail.com>
2020-04-22 21:05:40 -0500

    trying to add the lat and lng to the map

--- js/index.js
+++ js/index.js
@@ -1,34 +1,28 @@
 "use strict";
-
-(function () {
-  $(document).ready(function () {
-    console.log("DOM Ready");
-    function moveISS() {
-      $.getJSON("http://api.open-notify.org/iss-now.json?callback=?", function (
-        data
-      ) {
-        var lat = ["iss_position"]["latitude"];
-        var lon = ["iss_position"]["longitude"];
-
-        iss.setLatLng([lat, lon]);
-        isscirc.setLatLng([lat, lon]);
-        map.panTo([lat, lon], (animate = true));
-      });
-      setTimeout(moveISS, 5000);
-    }
-    var mymap = L.map("mapid").setView([51.505, -0.09], 13);
-
-    L.tileLayer(
-      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: "mapbox/streets-v11",
-        tileSize: 512,
-        zoomOffset: -1, 
-        accessToken: MAP_TOKEN,
-      }
-    ).addTo(mymap);
+$(document).ready(function () {
+  console.log("DOM Ready");
+  $.getJSON("http://api.open-notify.org/iss-now.json?callback=?", function (
+    data
+  ) {
+    var lat = data.iss_position.latitude;
+    var lng = data.iss_position.longitude;
+    console.log(lat, lng);
+    // See leaflet docs for setting up icons and map layers
+    // The update to the map is done here:
   });
-})();
+
+  var mymap = L.map("mapid").setView([lat, lng], 13);
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: MAP_TOKEN,
+    }
+  ).addTo(mymap);
+});
